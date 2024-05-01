cf7492e0931cfe68ad6ddb52c6932e6125ab86c8
Marion Pavaux <marion.pavaux@outlook.fr>
2024-05-01 19:50:40 +0200

    Refactor lightly namings and environment creation

--- MPI_CPU/src/execution.py
+++ MPI_CPU/src/execution.py
@@ -3,18 +3,18 @@
 import numpy as np
 import os
 
-#define a set of parameters (number of node, task per node)
-ProcNumbers = [[1,1],[1,2],[1,4],[2,4],[4,4],[8,4]]
+# define a set of parameters (number of node, task per node)
+ProcNumbers = [[1, 1], [1, 2], [1, 4], [2, 4], [4, 4], [8, 4]]
 
-#Clear previous data in Result file
-Result = open ("/export/home/grp08/monte_carlo/Result.dat", "w")
+# Clear previous data in Result file
+Result = open("results/result.dat", "w")
 Result.close()
-   
+
 for nb in ProcNumbers:
-    num = nb[0]*nb[1]
+    num = nb[0] * nb[1]
     # Copy template input file into txt object
-    Job = "Performance_monte_carlo.job"
-    inp = open( Job, "r" )
+    Job = "performance_monte_carlo.job"
+    inp = open(Job, "r")
     txt = inp.read()
     inp.close()
 
@@ -23,15 +23,14 @@ for nb in ProcNumbers:
     TaskNumber = "%d" % nb[1]
 
     # Replace reserved number of nodes with chosen values
-    newTxt = txt.replace( "NODESNUMBER", NodeNumber )
+    newTxt = txt.replace("NODESNUMBER", NodeNumber)
 
     # Replace reserved reserved number of tasks with chosen values
-    newTxt2 = newTxt.replace( "TASKSNUMBER", TaskNumber )
-
+    newTxt2 = newTxt.replace("TASKSNUMBER", TaskNumber)
 
     # Create jobname for submission and write info to screen
     Filename = "Job-%02d.job" % num
-    print ("Filename:", Filename)
+    print("Filename:", Filename)
 
     # Write input file for job submission
     out = open(Filename, "w")
@@ -40,6 +39,6 @@ for nb in ProcNumbers:
 
     # Submit to Slurm
 
-    command = 'sbatch Job-%02d.job' % num
+    command = "sbatch Job-%02d.job" % num
 
     os.system(command)
--- README.md
+++ README.md
@@ -1,28 +1,24 @@
 # High Performance Computing
+
 High Performance Computing projects on OpenMP, MPI and with GPUs
 
-## MPI_CPU 
+---
 
-A stochastic method to solve Laplace equation. 
+## MPI_CPU
 
-<ol>
-    <li>create an appropriate environment using conda</li>
-    conda create -n mpi_env anaconda python=3.9
-    <li>activate environment</li>
-    conda activate mpi_env
-    <li>install mpi4py</li>
-    conda install mpi4py
-</ol>
+A stochastic method to solve Laplace equation.
+
+### Environment creation
+
+Create an appropriate environment using conda:
+`bash
+    conda create -f MPI_CPU/environment.yml
+    `
 
 ## OpenMP_CPU
 
-Two sorting algorithms implemented in parallel. 
+Two sorting algorithms implemented in parallel.
 
 ## OpenCL_GPU
 
 Two image filtering algorithms implemented in parallel using GPUs.
-
-
-
-
-
